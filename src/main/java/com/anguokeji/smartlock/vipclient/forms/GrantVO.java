9a2e01210b0bbe2590aa353c50754203764477f0
bobdeng <bobdeng@163.com>
2019-01-21 21:50:49 +0800

    add grantvo

---
+++ src/main/java/com/anguokeji/smartlock/vipclient/forms/GrantVO.java
@@ -0,0 +1,50 @@
+package com.anguokeji.smartlock.vipclient.forms;
+
+
+public class GrantVO {
+    private String lockId;
+    private String user;
+    private int level;
+    private long start;
+    private long end;
+
+    public String getLockId() {
+        return lockId;
+    }
+
+    public void setLockId(String lockId) {
+        this.lockId = lockId;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public long getStart() {
+        return start;
+    }
+
+    public void setStart(long start) {
+        this.start = start;
+    }
+
+    public long getEnd() {
+        return end;
+    }
+
+    public void setEnd(long end) {
+        this.end = end;
+    }
+}
